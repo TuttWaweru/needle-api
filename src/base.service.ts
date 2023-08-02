3560b8a70d874609aba05b06965a1b578af48538
Tutt Waweru <tutt.waweru040@gmail.com>
2023-08-02 17:15:52 +0300

    Update base service formatTimeToAmPmOr24Hour function

--- src/base.service.ts
+++ src/base.service.ts
@@ -187,16 +187,16 @@ export class BaseService<U, T> implements BaseServiceInterface<U,T> {
         }
     }
 
-    formatTimeToAmPmOr24Hour(date, isAmPm: boolean = true) {
+    formatTimeToAmPmOr24Hour(date, isAmPm) {
         // Alternative method but for 24 hour system
         if (isAmPm) {
-            var hours = date.getHours();
-            var minutes = date.getMinutes();
-            var ampm = hours >= 12 ? 'pm' : 'am';
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+            const ampm = hours >= 12 ? 'pm' : 'am';
             hours = hours % 12;
             hours = hours ? hours : 12; // the hour '0' should be '12'
             minutes = minutes < 10 ? '0'+minutes : minutes;
-            var strTime = hours + ':' + minutes + ' ' + ampm;
+            const strTime = hours + ':' + minutes + ' ' + ampm;
             return strTime;
         } else {
             return date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
